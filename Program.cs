2ac055273d27332d989864bd4ebcfc9783b48892
ramiro-munoz-dev <ramimunoz1@gmail.com>
2026-02-04 15:06:50 -0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+class Program
+{
+    static bool EsPar(int numero)
+    {
+        return numero % 2 == 0;
+    }
+
+    static void Main()
+    {
+        int i = 1;
+        while (i <= 10)
+        {
+            Console.WriteLine(i + "->" + EsPar(i));
+            i++;
+        }
+    }
+}
